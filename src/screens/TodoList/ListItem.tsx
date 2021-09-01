8cd6ddc882263a5f260c9c57d3bc7381f54010af
namkyu koo(구남규) <rnrb555@gmail.com>
2021-09-02 06:49:55 +0900

    style/스타일수정

--- src/screens/TodoList/ListItem.tsx
+++ src/screens/TodoList/ListItem.tsx
@@ -1,92 +1,90 @@
-import React, { useRef, useState } from 'react'
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import React, { useRef, useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import styled from 'styled-components/native';
-import Icon from "react-native-vector-icons/MaterialIcons"
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { postTodoDelete } from '../../api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Ilist } from '../../../shared-interfaces';
 
-
 interface Icontent {
-    content: string
-    id: string
-    setList: React.Dispatch<React.SetStateAction<Ilist[]>>
+  content: string;
+  id: string;
+  setList: React.Dispatch<React.SetStateAction<Ilist[]>>;
 }
 
 const ListItem = ({ content, id, setList }: Icontent) => {
-    const handleDelete = () => {
-        setList((prevState: any) =>
-            prevState.filter((item: Ilist) => item.id !== id),
-        );
-    }
+  const handleDelete = () => {
+    setList((prevState: any) =>
+      prevState.filter((item: Ilist) => item.id !== id),
+    );
+  };
 
+  const handleEditContent = e => {
+    setList((prevState: Ilist[]) =>
+      prevState.map((todo: Ilist) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            content: e.target.value,
+          };
+        }
+        return todo;
+      }),
+    );
+  };
+  const textRef = useRef();
+  const handleFocus = (nextOne: any) => {
+    nextOne?.current?.focus();
+  };
 
-    const handleEditContent = (e) => {
-        setList((prevState: Ilist[]) =>
-            prevState.map((todo: Ilist) => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        content: e.target.value,
-                    };
-                }
-                return todo;
-            }),
-        );
-    }
-    const textRef = useRef()
-    const handleFocus = (nextOne: any) => {
-        nextOne?.current?.focus();
-    };
+  const handleChange = (text: string) => {
+    setList((prevState: Ilist[]) =>
+      prevState.map((todo: Ilist) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            content: text,
+          };
+        }
+        return todo;
+      }),
+    );
+  };
+  return (
+    <ListItemContainer>
+      <ContentWrap>
+        <TextInput ref={textRef} onChangeText={text => handleChange(text)}>
+          {content}
+        </TextInput>
+        <EditIcon onPress={() => handleFocus(textRef)}>
+          <Icon name="edit" size={20} />
+        </EditIcon>
+      </ContentWrap>
 
-    const handleChange = (text: string) => {
-        setList((prevState: Ilist[]) =>
-            prevState.map((todo: Ilist) => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        content: text,
-                    };
-                }
-                return todo;
-            }),
-        );
-    }
-    return (
-        <ListItemContainer>
-            <ContentWrap>
-                <TextInput ref={textRef} onChangeText={(text) => handleChange(text)}>{content}</TextInput>
-                <EditIcon onPress={() => handleFocus(textRef)}>
-                    <Icon name="edit" size={20} />
-                </EditIcon>
-            </ContentWrap>
-
-            <TouchableOpacity onPress={handleDelete}>
-                <Icon name="delete" size={25} />
-            </TouchableOpacity>
-        </ListItemContainer>
-    )
-
-}
+      <TouchableOpacity onPress={handleDelete}>
+        <Icon name="delete" size={25} />
+      </TouchableOpacity>
+    </ListItemContainer>
+  );
+};
 
 const ListItemContainer = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid rgba(0,0,0,0.6);
-    margin:10px 0;
-    border-radius: 5px;
-    padding: 30px 20px;
-`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.6);
+  margin: 10px 0;
+  border-radius: 5px;
+  padding: 30px 20px;
+`;
 
 const ContentWrap = styled.View`
-    flex-direction: row;
-    align-items: center;
-`
+  flex-direction: row;
+  align-items: center;
+`;
 
 const EditIcon = styled.TouchableOpacity`
-    margin-left: 20px;
-`
+  margin-left: 20px;
+`;
 
-
-export default ListItem
+export default ListItem;
--- src/screens/TodoList/index.tsx
+++ src/screens/TodoList/index.tsx
@@ -4,36 +4,33 @@ import TodoLists from './TodoLists';
 import AddTodoBtn from '../../components/AddTodoBtn';
 import { Ilist } from '../../../shared-interfaces';
 
-
-
 const TodoList: React.FC = () => {
-    const [list, setList] = useState<Ilist[]>([
-        {
-            id: "1",
-            isCheck: false,
-            createdAt: "2021-05-26T11:51:05.097Z",
-            content: "타입스크립트 공부하기"
-        },
-        {
-            id: "2",
-            isCheck: false,
-            createdAt: "2021-05-26T11:51:05.097Z",
-            content: "이력서 쓰기"
-        },
-    ])
+  const [list, setList] = useState<Ilist[]>([
+    {
+      id: '1',
+      isCheck: false,
+      createdAt: '2021-05-26T11:51:05.097Z',
+      content: '타입스크립트 공부하기',
+    },
+    {
+      id: '2',
+      isCheck: false,
+      createdAt: '2021-05-26T11:51:05.097Z',
+      content: '이력서 쓰기',
+    },
+  ]);
 
-    return (
-        <Container>
-            <TodoLists list={list} setList={setList} />
-            <AddTodoBtn list={list} setList={setList} />
-        </Container>
-    );
+  return (
+    <Container>
+      <TodoLists list={list} setList={setList} />
+      <AddTodoBtn list={list} setList={setList} />
+    </Container>
+  );
 };
 
 const Container = styled.View`
-    padding:0 20px;
-    flex: 1;
-`
-
+  padding: 0 20px;
+  flex: 1;
+`;
 
 export default TodoList;
